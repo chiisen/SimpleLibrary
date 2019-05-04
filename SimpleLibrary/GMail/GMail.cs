249a93c249a3b9389d2e8abc2df3713ab357d270
chiisen <chiisen@hotmail.com>
2019-05-05 03:28:10 +0800

    1. 新增 GMail 寄信功能 2. 新增 Zip 壓縮檔案功能

---
+++ SimpleLibrary/GMail/GMail.cs
@@ -0,0 +1,99 @@
+﻿using Autofac;
+using SimpleLibrary.Logger;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Net.Mail;
+
+namespace SimpleLibrary.GMail
+{
+    public class GMail
+    {
+        /// <summary>
+        /// 發信人的 Email
+        /// </summary>
+        private readonly string _EmailAddress = "";
+
+        /// <summary>
+        /// 發信人的 Email 密碼
+        /// </summary>
+        private readonly string _EmailPassword = "";
+
+        private ILogger _Logger = new ConsoleLogger();
+
+        private void Print(string msg, Color color)
+        {
+            if (_Logger != null)
+            {
+                _Logger.Print(msg, color);
+            }
+        }
+
+        private void InitLogger(ContainerBuilder builder)
+        {
+            if (builder != null)
+            {
+                IContainer container_ = builder.Build();
+                _Logger = container_.Resolve<ILogger>();
+            }
+        }
+
+        public GMail(string emailAddress, string emailPassword, ContainerBuilder builder = null)
+        {
+            _EmailAddress  = emailAddress;
+            _EmailPassword = emailPassword;
+
+            InitLogger(builder);
+        }
+
+        public void SendMessage(string displayName, string subject, string body, List<string> ToAdd)
+        {
+            MailMessage mailMessage_ = new MailMessage
+            {
+                // 前面是發信 email 後面是顯示的名稱
+                From = new MailAddress(_EmailAddress, displayName)
+            };
+
+            // 收信者 email
+            for (int i = 0; i < ToAdd.Count; ++i)
+            {
+                mailMessage_.To.Add(ToAdd[i]);
+            }
+
+            if (mailMessage_.To.Count > 0)
+            {
+
+                // 設定優先權
+                mailMessage_.Priority = MailPriority.Normal;
+
+                // 標題
+                mailMessage_.Subject = subject;
+
+                // 內容
+                mailMessage_.Body = body;
+
+                // 內容使用 html
+                mailMessage_.IsBodyHtml = true;
+
+                // 設定 gmail 的 smtp (這是 google 的)
+                SmtpClient smtpClient_ = new SmtpClient("smtp.gmail.com", 587)
+                {
+                    // 您在 gmail 的帳號密碼
+                    Credentials = new NetworkCredential(_EmailAddress, _EmailPassword),
+
+                    // 開啟ssl
+                    EnableSsl = true
+                };
+
+                // 發送郵件
+                smtpClient_.Send(mailMessage_);
+
+                // 放掉宣告出來的MySmtp
+                smtpClient_ = null;
+            }
+
+            // 放掉宣告出來的 mail
+            mailMessage_.Dispose();
+        }
+    }
+}
---
+++ SimpleLibrary/Zip/Zip.cs
@@ -0,0 +1,131 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using SimpleLibrary.Logger;
+using System;
+using System.Drawing;
+using System.IO;
+using Autofac;
+
+namespace SimpleLibrary.Zip
+{
+    public class Zip
+    {
+        private ILogger _Logger = new ConsoleLogger();
+
+        private void Print(string msg, Color color)
+        {
+            if (_Logger != null)
+            {
+                _Logger.Print(msg, color);
+            }
+        }
+
+        private void InitLogger(ContainerBuilder builder)
+        {
+            if (builder != null)
+            {
+                IContainer container_ = builder.Build();
+                _Logger = container_.Resolve<ILogger>();
+            }
+        }
+
+        public Zip(ContainerBuilder builder = null)
+        {
+            InitLogger(builder);
+        }
+
+        /// <summary>
+        /// 檢查並提示 S3 的下載網址不能有 ' 分號
+        /// </summary>
+        /// <param name="filePath">上傳 S3 的檔案路徑</param>
+        void CheckS3Path(string filePath)
+        {
+            string path_ = filePath;
+            if (path_.Contains("'") == true)
+            {
+                // S3 的下載網址不能有 ' 分號
+                string error_ = $@"S3 的下載網址不能有 ' 分號 path = {path_}";
+                Print(error_, Color.OrangeRed);
+            }
+        }
+
+        /// <summary>
+        /// 壓縮指定目錄為 zip 檔案
+        /// </summary>
+        /// <param name="outputZipPath">輸出的 zip 檔案路徑</param>
+        /// <param name="inputDirectory">要壓縮的目錄路徑</param>
+        void ZipTo(string outputZipPath, string inputDirectory)
+        {
+            // 修正 S3 的下載網址不能有 ' 分號
+            CheckS3Path(outputZipPath);
+
+            ZipOutputStream zipStream_ = new ZipOutputStream(File.Create(outputZipPath));
+            zipStream_.SetLevel(9);
+
+            ZipFolder(inputDirectory, inputDirectory, zipStream_);
+
+            zipStream_.Finish();
+            zipStream_.Close();
+        }
+
+        /// <summary>
+        /// 壓縮指定目錄
+        /// </summary>
+        /// <param name="rootFolder">指定要壓縮的目錄同 currentFolder</param>
+        /// <param name="currentFolder">指定要壓縮的目錄同 rootFolder</param>
+        /// <param name="zipStream">ZipOutputStream 的參考</param>
+        private static void ZipFolder(string rootFolder, string currentFolder, ZipOutputStream zipStream)
+        {
+            string[] SubFolders_ = Directory.GetDirectories(currentFolder);
+
+            foreach (string Folder in SubFolders_)
+            {
+                ZipFolder(rootFolder, Folder, zipStream);
+            }
+
+            string relativePath_ = currentFolder.Substring(rootFolder.Length) + "/";
+
+            if (relativePath_.Length > 1)
+            {
+                ZipEntry dirEntry_;
+
+                dirEntry_ = new ZipEntry(relativePath_)
+                {
+                    DateTime = DateTime.Now
+                };
+            }
+
+            foreach (string file in Directory.GetFiles(currentFolder))
+            {
+                AddFileToZip(zipStream, relativePath_, file);
+            }
+        }
+
+        /// <summary>
+        /// 新增檔案到 zip 檔案內
+        /// </summary>
+        /// <param name="zipStream"></param>
+        /// <param name="relativePath"></param>
+        /// <param name="file"></param>
+        private static void AddFileToZip(ZipOutputStream zipStream, string relativePath, string file)
+        {
+            byte[] buffer_ = new byte[4096];
+            string fileRelativePath_ = (relativePath.Length > 1 ? relativePath : string.Empty) + Path.GetFileName(file);
+            ZipEntry entry_ = new ZipEntry(fileRelativePath_)
+            {
+                DateTime = DateTime.Now
+            };
+            zipStream.PutNextEntry(entry_);
+
+            using (FileStream fs = File.OpenRead(file))
+            {
+                int sourceBytes_;
+
+                do
+                {
+                    sourceBytes_ = fs.Read(buffer_, 0, buffer_.Length);
+                    zipStream.Write(buffer_, 0, sourceBytes_);
+                } while (sourceBytes_ > 0);
+            }
+        }
+    }
+}
