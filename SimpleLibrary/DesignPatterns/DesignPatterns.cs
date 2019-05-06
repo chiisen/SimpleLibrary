6d517c22e0b3902f3a74b9632937e5c95c257ce8
chiisen <chiisen@hotmail.com>
2019-05-06 18:39:06 +0800

    新增 Singleton 與 Extension Method

---
+++ SimpleLibrary/DesignPatterns/DesignPatterns.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace SimpleLibrary.DesignPatterns
+{
+    /// <summary>
+    /// 給人繼承成為獨體
+    /// </summary>
+    /// <typeparam name="T">要繼承的類別</typeparam>
+    public class Singleton<T> where T : class, new()
+    {
+        protected Singleton()
+        {
+            Debug.Assert(null == _instance);
+        }
+        private static readonly T _instance = new T();
+
+        public static T Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
---
+++ SimpleLibrary/FP/FP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleLibrary.FP
+{
+    /// <summary>
+    /// Extension Method
+    /// 因為 IEnumerable 沒有 ForEach()，只有 List 才有 ForEach()，因此我們必須先 ToList()
+    /// 所以就來替 IEnumerable 打造一個 ForEach()
+    /// </summary>
+    public static class Extensions
+    {
+        internal static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (T element in source)
+                action(element);
+        }
+    }
+}
